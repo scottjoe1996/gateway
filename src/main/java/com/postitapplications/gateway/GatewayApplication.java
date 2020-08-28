e88a8d8fd921bba327d1b3bccd2609ebb1c19ca8
Joseph Anthony Scott <scottjoe1996@yahoo.co.uk>
2020-08-28 17:06:17 +0100

    fixed eureka/zuul config, ITS WORKING

--- src/main/java/com/postitapplications/gateway/GatewayApplication.java
+++ src/main/java/com/postitapplications/gateway/GatewayApplication.java
@@ -2,10 +2,10 @@ package com.postitapplications.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
